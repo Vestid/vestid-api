f665ae49393c247a29747c2e492b1e270a0ac517
Dallin Parker <dallin.r.parker@gmail.com>
2018-01-27 18:14:33 -0500

    basic server is running with linting and prettier

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-require('dotenv').config();
+const projectname = "vestid API v1.0";
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const massive = require("massive");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+
+const app = (module.exports = express());
+app.set("port", process.env.PORT || 3030);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false
+  })
+);
+
+app.listen(app.get("port"), () =>
+  console.log(`${projectname} running on `, app.get("port"))
+);
