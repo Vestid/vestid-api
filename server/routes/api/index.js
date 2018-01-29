ac2853e80930359a5e18edf2f012caf746ecf691
Dallin Parker <dallin.r.parker@gmail.com>
2018-01-29 09:19:24 -0500

    WIP: need to refactor routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,27 +1,19 @@
 const router = require('express').Router()
 const passport = require('./authentication/passport')
+console.log(passport.authenticate)
 
 // router.post('/login', passport.authenticate('local', {
 //     successRedirect: '/success'
 // }))
-// router.post('/login', (req, res, next) => {
-//     res.end('fuck ya!!!')
-// })
-
-router.post(
-  '/login',
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/failed'
-  })
-)
-
-router.get('/failed', () => console.log('you failed in passport'))
-router.get('/', () => console.log('succssfully FUCK YA!'))
-
 // router.get('/success', (req, res, next) => {
 //     console.log('youre in the right fucking thing')
 // })
+router.post('/login', passport.authenticate('local'), (req, res, next) => {
+  console.log('request after: ', req.body)
+})
+
+router.get('/failed', () => console.log('you failed in passport'))
+router.get('/', () => console.log('succssfully used passport!'))
 
 // router.use("/login", require("./authentication/auth"));
 // router.use('/success', require('./authentication/auth'))
